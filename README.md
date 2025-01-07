2c0557785c14d73cc86fe8e7cae18f246a0a36c9
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-01-08 00:03:43 +0100

    cc

--- README.md
+++ README.md
@@ -39,21 +39,15 @@ npm start
 
 <h3>Landing page</h3>
 
-
 ![landing page](https://github.com/user-attachments/assets/9e1ef65f-ca21-4615-9820-f8f00204ad85)
-
 
 <h3>Shop page</h3>
 
-
 ![shop page](https://github.com/user-attachments/assets/e2935c47-9b53-4d26-9221-05451102260c)
-
 
 <h3>Single product page</h3>
 
-
 ![single product page](https://github.com/user-attachments/assets/815eaa98-150d-4847-9339-5140745c66ba)
-
 
 <h3>Cart page</h3>
 
@@ -63,7 +57,6 @@ npm start
 
 ![login page](https://github.com/user-attachments/assets/4903e803-9253-4212-be4d-cfa14e010fb3)
 
-
 <h3>Register page</h3>
 
 ![register page](https://github.com/user-attachments/assets/a2c5f5cb-d03f-46c8-b43a-edd3876e3001)
@@ -72,11 +65,9 @@ npm start
 
 ![user profile page](https://github.com/user-attachments/assets/5786d46b-29a8-44c2-ad52-3a794ce954c9)
 
-
 <h3>Order history page</h3>
 
 ![order history page](https://github.com/user-attachments/assets/57259617-6c4e-4efd-84ad-961ee0a9b9e4)
-
 
 <h3>Single order history page</h3>
 
@@ -85,7 +76,6 @@ npm start
 <h3>Checkout page</h3>
 
 ![checkout page](https://github.com/user-attachments/assets/0dc47027-1bf7-4b96-bff2-73867d6892a9)
-
 
 <h3>Search page</h3>
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,30 +7,36 @@ import SidebarMenu from "./SidebarMenu";
 import { useState } from "react";
 
 const Header = () => {
-  const [ isSidebarOpen, setIsSidebarOpen ] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   return (
     <>
-    <header className="max-w-screen-2xl flex text-center justify-between items-center py-4 px-5 text-black mx-auto max-sm:px-5 max-[400px]:px-3">
-      <HiBars3 className="text-2xl max-sm:text-xl mr-20 max-lg:mr-0 cursor-pointer" onClick={() => setIsSidebarOpen(true)} />
-      <Link
-        to="/"
-        className="text-4xl font-light tracking-[1.08px] max-sm:text-3xl max-[400px]:text-2xl"
-      >
-        FASHION
-      </Link>
-      <div className="flex gap-4 items-center max-sm:gap-2">
-        <Link to="/search">
-          <HiOutlineMagnifyingGlass className="text-2xl max-sm:text-xl" />
+      <header className="max-w-screen-2xl flex text-center justify-between items-center py-4 px-5 text-black mx-auto max-sm:px-5 max-[400px]:px-3">
+        <HiBars3
+          className="text-2xl max-sm:text-xl mr-20 max-lg:mr-0 cursor-pointer"
+          onClick={() => setIsSidebarOpen(true)}
+        />
+        <Link
+          to="/"
+          className="text-4xl font-light tracking-[1.08px] max-sm:text-3xl max-[400px]:text-2xl"
+        >
+          KAK Abaya
         </Link>
-        <Link to="/login">
-          <HiOutlineUser className="text-2xl max-sm:text-xl" />
-        </Link>
-        <Link to="/cart">
-          <HiOutlineShoppingBag className="text-2xl max-sm:text-xl" />
-        </Link>
-      </div>
-    </header>
-    <SidebarMenu isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen} />
+        <div className="flex gap-4 items-center max-sm:gap-2">
+          <Link to="/search">
+            <HiOutlineMagnifyingGlass className="text-2xl max-sm:text-xl" />
+          </Link>
+          <Link to="/login">
+            <HiOutlineUser className="text-2xl max-sm:text-xl" />
+          </Link>
+          <Link to="/cart">
+            <HiOutlineShoppingBag className="text-2xl max-sm:text-xl" />
+          </Link>
+        </div>
+      </header>
+      <SidebarMenu
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
     </>
   );
 };
