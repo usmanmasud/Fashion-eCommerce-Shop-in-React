945552a9ca9cec7ed998ef7126a971e5618f73c2
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-01-17 00:15:42 +0100

    n

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Banner = () => {
   return (
     <div className="banner w-full flex flex-col justify-end items-center max-sm:h-[550px] max-sm:gap-2">
-      <h2 className="text-white text-center text-6xl font-bold tracking-[1.86px] leading-[60px] max-sm:text-4xl max-[400px]:text-3xl">
+      <h2 className="text-white text-center text-6xl font-bold tracking-[1.86px] leading-[60px] max-sm:text-4xl max-[400px]:text-2xl">
         Discover the Best <br />
         Abaya
       </h2>
